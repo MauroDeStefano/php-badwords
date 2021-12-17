d0f59bbe664dc699b01aeb7086dfb0d5151684ea
MauroDeStefano <maurodestefano@gmail.com>
2021-12-17 13:31:04 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$frase = "Questa è la frase in cui eviedentemente c'è qualche parola che non piace, quindi non farti problemi a censurare qualcosa.";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php echo str_replace($_GET['toClear'] , "***", $frase)?>
+    </h1>
+
+</body>
+</html>
